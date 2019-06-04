9c945efe7327d27f29cf7a7273369dc3167fa32d
Antony Nganga <antonynganga05@gmail.com>
2019-06-04 14:04:16 +0300

    Create main layout

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.weatherapp;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.back)
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+//        Picasso.get().load(R.drawable.weather).into(back);
+    }
+}
