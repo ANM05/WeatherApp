8579299c62f47df2e00974e1b3d872915617c7ed
Antony Nganga <antonynganga05@gmail.com>
2019-06-11 11:57:50 +0300

    Add internet and location permissions to manifest

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.weatherapp;
 
 //import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +15,13 @@ import com.squareup.picasso.Picasso;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.back)
     ImageView back;
+    @BindView(R.id.btnfind)
+    Button mBtnFind;
+    @BindView(R.id.editText)
+    EditText mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,14 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
+    }
+
+    public void onClick(View v){
+        if(v==mBtnFind){
+            String location= mLocation.getText().toString();
+            Intent intent= new Intent(MainActivity.this, WeatherActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
